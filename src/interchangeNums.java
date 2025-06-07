5c1c11211b1296c52861fda0749f198ba21b6c48
spkara25 <spkarajagi2006@gmail.com>
2025-06-07 17:25:51 +0530

    nextPrgm

--- src/interchangeNums.java
+++ src/interchangeNums.java
@@ -3,6 +3,20 @@ import java.util.Scanner;
 public class interchangeNums {
     public static void main(String[] args)
     {
+        int a, b;
+        Scanner sc = new Scanner(System.in);
+        int temp;
+        System.out.println("Enter two numbers: ");
+        a = sc.nextInt();
+        b = sc.nextInt();
+        System.out.println("The non-swapped numbers are: ");
+        System.out.printf("%d %d", a, b);
+        System.out.printf("\n");
+        temp = a; //temp = 5
+        a = b; //a = 10
+        b = temp; // b= 5;
+        System.out.println("The swapped characters are: ");
+        System.out.printf("%d %d", a, b);
 
     }
 }
